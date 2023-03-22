c374b5711ed0c3b880e6515a8c32cf3cbca66041
Lucas Schwiderski <lucas@lschwiderski.de>
2023-03-22 22:26:04 +0100

    WIP

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+use cc::Build;
 use fs_extra::dir;
 use fs_extra::dir::CopyOptions;
 use std::env;
@@ -6,33 +7,85 @@ use std::process::{Command, Stdio};
 
 const LIB_NAME: &str = "luajit";
 const LUAJIT_HEADERS: [&str; 4] = ["lua.h", "lualib.h", "lauxlib.h", "luajit.h"];
+const LUAJIT_SRC: [&str; 69] = [
+    // LJCORE_O
+    "lj_assert.c",
+    "lj_gc.c",
+    "lj_err.c",
+    "lj_char.c",
+    "lj_bc.c",
+    "lj_obj.c",
+    "lj_buf.c",
+    "lj_str.c",
+    "lj_tab.c",
+    "lj_func.c",
+    "lj_udata.c",
+    "lj_meta.c",
+    "lj_debug.c",
+    "lj_prng.c",
+    "lj_state.c",
+    "lj_dispatch.c",
+    "lj_vmevent.c",
+    "lj_vmmath.c",
+    "lj_strscan.c",
+    "lj_strfmt.c",
+    "lj_strfmt_num.c",
+    "lj_serialize.c",
+    "lj_api.c",
+    "lj_profile.c",
+    "lj_lex.c",
+    "lj_parse.c",
+    "lj_bcread.c",
+    "lj_bcwrite.c",
+    "lj_load.c",
+    "lj_ctype.c",
+    "lj_cdata.c",
+    "lj_cconv.c",
+    "lj_ccall.c",
+    "lj_ccallback.c",
+    "lj_carith.c",
+    "lj_clib.c",
+    "lj_cparse.c",
+    "lj_lib.c",
+    "lj_ir.c",
+    "lj_opt_mem.c",
+    "lj_opt_fold.c",
+    "lj_opt_narrow.c",
+    "lj_opt_dce.c",
+    "lj_opt_loop.c",
+    "lj_opt_split.c",
+    "lj_opt_sink.c",
+    "lj_mcode.c",
+    "lj_snap.c",
+    "lj_record.c",
+    "lj_crecord.c",
+    "lj_ffrecord.c",
+    "lj_asm.c",
+    "lj_trace.c",
+    "lj_gdbjit.c",
+    "lj_alloc.c",
+    // LJLIB_O
+    "lib_aux.c",
+    "lib_base.c",
+    "lib_math.c",
+    "lib_string.c",
+    "lib_table.c",
+    "lib_io.c",
+    "lib_os.c",
+    "lib_package.c",
+    "lib_debug.c",
+    "lib_bit.c",
+    "lib_jit.c",
+    "lib_ffi.c",
+    "lib_buffer.c",
+    "lib_init.c",
+];
 
-fn main() {
-    let luajit_dir = format!("{}/luajit", env!("CARGO_MANIFEST_DIR"));
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let src_dir = format!("{}/luajit/src", out_dir);
-    let lib_path = format!("{}/lib{}.a", &src_dir, LIB_NAME);
-
-    dbg!(&luajit_dir);
-    dbg!(&out_dir);
-    dbg!(&src_dir);
-    dbg!(&lib_path);
-
-    let mut copy_options = CopyOptions::new();
-    copy_options.overwrite = true;
-
-    dir::copy(&luajit_dir, &out_dir, &copy_options).expect("Failed to copy LuaJIT source");
-
+fn build_host(src_dir: &str) {
     let mut buildcmd = Command::new("make");
     buildcmd.current_dir(&src_dir);
     buildcmd.stderr(Stdio::inherit());
     buildcmd.arg("--no-silent");
-    buildcmd.arg("BUILDMODE=static");
-
-    if env::var("CARGO_CFG_WINDOWS").is_ok() {
-        buildcmd.arg("TARGET_SYS=Windows");
-        buildcmd.arg("CROSS=x86_64-w64-mingw32-");
-    }
 
     if cfg!(target_pointer_width = "32") {
         buildcmd.arg("HOST_CC='gcc -m32'");
@@ -51,11 +104,52 @@ fn main() {
     {
         panic!("Failed to build luajit");
     }
+}
+
+fn build_msvc(src_dir: &str, out_dir: &str) {
+    let mut cc = Build::new();
+    cc.warnings(false)
+        .extra_warnings(false)
+        .out_dir(out_dir)
+        .cargo_metadata(true)
+        .static_flag(true)
+        .force_frame_pointer(false);
+
+    for f in LUAJIT_SRC {
+        cc.file(format!("{src_dir}/{f}"));
+    }
+
+    cc.compile(LIB_NAME);
+}
+
+fn main() {
+    let luajit_dir = format!("{}/luajit", env!("CARGO_MANIFEST_DIR"));
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let src_dir = format!("{}/luajit/src", out_dir);
+
+    dbg!(&luajit_dir);
+    dbg!(&out_dir);
+    dbg!(&src_dir);
+
+    let mut copy_options = CopyOptions::new();
+    copy_options.overwrite = true;
+
+    dir::copy(&luajit_dir, &out_dir, &copy_options).expect("Failed to copy LuaJIT source");
+
+    // The first run builds with and for the host architecture.
+    // This also creates all the tools and generated sources that a compilation needs.
+    build_host(&src_dir);
+
+    // Then, for cross-compilation, we can utilize those generated
+    // sources to re-compile just the library.
+    if env::var("CARGO_CFG_WINDOWS").is_ok() {
+        build_msvc(&src_dir, &out_dir);
+    }
 
     println!("cargo:lib-name={}", LIB_NAME);
     println!("cargo:include={}", src_dir);
-    println!("cargo:rustc-link-search=native={}", src_dir);
-    println!("cargo:rustc-link-lib=static={}", LIB_NAME);
+    println!("cargo:rustc-link-search={}", out_dir);
+    println!("cargo:rustc-link-lib={}", LIB_NAME);
 
     let mut bindings = bindgen::Builder::default();
 
